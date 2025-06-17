2fb41d5c9f4119fff728e565fc85fff0d28beb74
LunaB28 <lunab2815@gmail.com>
2025-06-17 02:33:42 +0000

    primeros cambios

--- src/admin.py
+++ src/admin.py
@@ -1,16 +1,45 @@
 import os
 from flask_admin import Admin
-from models import db, User
+from models import db, User, Planet, Character, Favorite_Planet, Favorite_Character
 from flask_admin.contrib.sqla import ModelView
+
+
+class UserModelView(ModelView):
+    column_auto_select_related = True
+    column_list = ['id', 'user', 'first_name', 'last_name',
+                   'subscription_date', 'email', 'is_active', 'fav_planet', 'fav_character']
+
+class PlanetModelView(ModelView):  
+    column_auto_select_related = True
+    column_list = ['id', 'name', 'weather', 'native_characters', 'favorite']
+
+class CharacterModelView(ModelView):  
+    column_auto_select_related = True
+    column_list = ['id', 'name', 'species',
+                   'planet_origin', 'native_of', 'favorites']
+    
+class FavoritePlanetsModelView(ModelView):  
+    column_auto_select_related = True
+    column_list = ['id', 'user_id', 'planet_id',
+                   'favorite_planet_by', 'planet_inf']
+
+class FavoriteCharactersModelView(ModelView):  
+    column_auto_select_related = True
+    column_list = ['id', 'user_id', 'character_id',
+                   'favorite_character_by', 'character_inf']
+
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
-    
     # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
+    admin.add_view(UserModelView(User, db.session))
+    admin.add_view(PlanetModelView(Planet, db.session))
+    admin.add_view(CharacterModelView(Character, db.session))
+    admin.add_view(FavoritePlanetsModelView(Favorite_Planet, db.session))
+    admin.add_view(FavoriteCharactersModelView(Favorite_Character, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/models.py
+++ src/models.py
@@ -14,15 +14,18 @@ class User(db.Model):
     last_name: Mapped[str] = mapped_column(
         String(25), unique=True, nullable=False)
     subscription_date: Mapped[str] = mapped_column(
-        String(50), unique=True, nullable=False)
+        String(50), nullable=False)
     email: Mapped[str] = mapped_column(
         String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
     fav_planet: Mapped[list['Favorite_Planet']]=relationship(
-        back_populates='favorite_by', cascade='all, delete-orphan')
-    fav_character: Mapped[list['Favorite_Character']]=relationship(
-        back_populates='favorite__by', cascade='all, delete-orphan')
+        back_populates='favorite_planet_by', cascade='all, delete-orphan')
+    fav_character: Mapped[list['Favorite_Character']] = relationship(
+        back_populates='favorite_character_by', cascade='all, delete-orphan')
+    
+    def __str__(self):
+        return f'User {self.user}'
 
 
 class Planet(db.Model):
@@ -36,33 +39,40 @@ class Planet(db.Model):
     favorite: Mapped[list['Favorite_Planet']] = relationship(
         back_populates='planet_inf', cascade='all, delete-orphan')
 
+    def __str__(self):
+        return f'Planet {self.name}'
+
 
 class Character(db.Model):
     __tablename__= 'character'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(25), unique=True, nullable=False)
     species: Mapped[str] = mapped_column(
-        String(25), unique=True, nullable=False)
-    planet_origin: Mapped[int] = mapped_column(ForeignKey(Planet.id))
-    native_of: Mapped[Planet]=relationship(
+        String(25), nullable=False)
+    planet_origin: Mapped[int] = mapped_column(ForeignKey('planet.id'))
+    native_of: Mapped[Planet] = relationship(
         back_populates='native_characters')
     favorites: Mapped[list['Favorite_Character']] = relationship(
         back_populates='character_inf', cascade='all, delete-orphan')
+    def __str__(self):
+        return f'{self.name}'
     
 class Favorite_Planet(db.Model):
+    __tablename__ = 'favorite_planet'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    user_id: Mapped[int]= mapped_column(ForeignKey(User.id))
-    planet_id: Mapped[int]=mapped_column(ForeignKey(Planet.id))
-    favorite_by: Mapped[User]=relationship(
+    user_id: Mapped[int]= mapped_column(ForeignKey('user.id'))
+    planet_id: Mapped[int]=mapped_column(ForeignKey('planet.id'))
+    favorite_planet_by: Mapped[User]=relationship(
         back_populates='fav_planet')
     planet_inf: Mapped[Planet]=relationship(
         back_populates='favorite')
 
 class Favorite_Character(db.Model):
+    __tablename__ = 'favorite_character'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    user_id: Mapped[int]= mapped_column(ForeignKey(User.id))
-    character_id: Mapped[int]=mapped_column(ForeignKey(Character.id))
-    favorite__by: Mapped[User]=relationship(
+    user_id: Mapped[int]= mapped_column(ForeignKey('user.id'))
+    character_id: Mapped[int]=mapped_column(ForeignKey('character.id'))
+    favorite_character_by: Mapped[User] = relationship(
         back_populates='fav_character')
-    character_inf: Mapped[Planet]=relationship(
+    character_inf: Mapped[Character]=relationship(
         back_populates='favorites')
